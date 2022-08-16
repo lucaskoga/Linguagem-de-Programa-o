083e2676a5c0963b932bc3b2fb660b49d9a2154c
Lucas Koga <54293587+lucaskoga@users.noreply.github.com>
2022-08-16 18:04:12 -0300

    Create Principal.java

---
+++ Atividade Avaliativa/SomaDiretorio-Parelelismo/Principal.java
@@ -0,0 +1,17 @@
+import java.io.File;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        //Soma Convencional
+        File file = new File("C:/Users/User/Desktop/Atividade");
+        long inicio = System.currentTimeMillis();
+        SomaByte soma = new SomaByte(file);
+        long set = soma.compute();
+        System.out.println("Soma de Byte convensional: "+ set);
+        System.out.println("Soma de Convertido em MegaByte: "+ (double)set / (1024 * 1024));
+        long fim = System.currentTimeMillis();
+        System.out.println("Tempo de Compilacao convensional: "+ (fim - inicio));
+
+    }
+}
